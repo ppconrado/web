ee8c2b164dc947700258dc0f9a907a241ea4c7a5
ppconrado <ppconrado@yahoo.com.br>
2022-05-06 18:33:24 -0300

    frontend done

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -36,17 +37,36 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        // Passa a funcao "setFeedbackType" do "useState" pela propriedade "onFeedbackTypeChanged" para o componente <FeedbackTypeStep />
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType} />
+        <>
+          {!feedbackType ? (
+            // Passa a funcao "setFeedbackType" do "useState" pela propriedade "onFeedbackTypeChanged" para o componente <FeedbackTypeStep />
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
-        Design by
+        Design by :
         <a
           className="underline underline-offset-2"
           href="https://github.com/ppconrado/"
